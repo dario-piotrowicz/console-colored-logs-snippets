315f3509d886142bc5efb69ca6adf84d05fcd916
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-10-09 15:08:56 +0100

    make enable/disable highlights commands work

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,6 @@
 import * as vscode from 'vscode';
 import { foregroundColorCodes, resetCode } from './colors';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('"console-colored-logs" extension active');
-
-	context.subscriptions.push(vscode.commands.registerCommand('console-colored-logs.enableHighlights', () => {
-    // TODO: this needs to update the settings
-    vscode.window.showInformationMessage('console-colored-logs highlights enabled!');
-	}));
-
-  context.subscriptions.push(vscode.commands.registerCommand('console-colored-logs.disableHighlights', () => {
-    // TODO: this needs to update the settings
-		vscode.window.showInformationMessage('console-colored-logs highlights disabled!');
-	}));
-
   // TODO: there need to be different styles for the different logs
   const DEFAULT_STYLE = {
     color: "#fff",
@@ -26,28 +13,46 @@ export function activate(context: vscode.ExtensionContext) {
 
   const decorationType = vscode.window.createTextEditorDecorationType(DEFAULT_STYLE);
 
-  vscode.workspace.onDidChangeTextDocument(() => {
-    const showHighlights = !!vscode.workspace.getConfiguration('console-colored-logs').get('showHighlights');
+export function activate(context: vscode.ExtensionContext) {
+	console.log('"console-colored-logs" extension active');
 
-    if (!showHighlights || !vscode.window.activeTextEditor?.document) return;
+	context.subscriptions.push(vscode.commands.registerCommand('console-colored-logs.enableHighlights', async () => {
+    await vscode.workspace.getConfiguration('console-colored-logs').update('showHighlights', true, true);
+    vscode.window.showInformationMessage('console-colored-logs highlights enabled');
+    updateActiveTextEditorHighlights();
+	}));
 
-    const text = vscode.window.activeTextEditor.document.getText();
+  context.subscriptions.push(vscode.commands.registerCommand('console-colored-logs.disableHighlights', async () => {
+    await vscode.workspace.getConfiguration('console-colored-logs').update('showHighlights', false, true);
+		vscode.window.showInformationMessage('console-colored-logs highlights disabled');
+    vscode.window.activeTextEditor?.setDecorations(decorationType, []);
+    updateActiveTextEditorHighlights();
+	}));
 
-    vscode.window.activeTextEditor.setDecorations(decorationType, []);
-    let match;
-    const pattern = new RegExp(
-      `console\\.log\\(\`(?:${
-        foregroundColorCodes.map(code => code.replace('[', '\\[')).join('|')})[\\s\\S]*?${resetCode.replace('[', '\\[')
-      }\`\\);`
-      , 'g');
-    const ranges: vscode.Range[] = [];
-    while (match = pattern.exec(text)) {
-        const startPos = vscode.window.activeTextEditor.document.positionAt(match.index);
-        const endPos = vscode.window.activeTextEditor.document.positionAt(match.index + match[0].length);
-        ranges.push(new vscode.Range(startPos, endPos));
-    }
-    vscode.window.activeTextEditor.setDecorations(decorationType, ranges);
-  });
+  vscode.workspace.onDidChangeTextDocument(updateActiveTextEditorHighlights);
+}
+
+function updateActiveTextEditorHighlights() {
+  const showHighlights = !!vscode.workspace.getConfiguration('console-colored-logs').get('showHighlights');
+
+  if (!showHighlights || !vscode.window.activeTextEditor?.document) return;
+
+  const text = vscode.window.activeTextEditor.document.getText();
+
+  vscode.window.activeTextEditor.setDecorations(decorationType, []);
+  let match;
+  const pattern = new RegExp(
+    `console\\.log\\(\`(?:${
+      foregroundColorCodes.map(code => code.replace('[', '\\[')).join('|')})[\\s\\S]*?${resetCode.replace('[', '\\[')
+    }\`\\);`
+    , 'g');
+  const ranges: vscode.Range[] = [];
+  while (match = pattern.exec(text)) {
+      const startPos = vscode.window.activeTextEditor.document.positionAt(match.index);
+      const endPos = vscode.window.activeTextEditor.document.positionAt(match.index + match[0].length);
+      ranges.push(new vscode.Range(startPos, endPos));
+  }
+  vscode.window.activeTextEditor.setDecorations(decorationType, ranges);
 }
 
 export function deactivate() {}
