d221b86ef71f4cc58e942d1c5bb0d30c37897a24
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-10-29 11:06:35 +0100

    add test to make sure non-matching logs are not matched

--- src/__tests__/collect-decoration-types-with-ranges/test.ts
+++ src/__tests__/collect-decoration-types-with-ranges/test.ts
@@ -17,6 +17,20 @@ describe('collectDecorationTypesWithNumRanges', () => {
       expect(result).toEqual(expectedResult);
     });
 
+    it.only('should not match logs not following the snippets format', () => {
+      const result = collectDecorationTypesWithNumRanges([
+        'console.log(``);',
+        'console.log(`this is a test`);',
+        'console.log(`this is a \\x1b[41m test \\x1b[0m`)',
+        'console.log(`this is a`, `\\x1b[41m test \\x1b[0m``);',
+        'console.log(\'\\x1b[41m test \\x1b[0m\')',
+        'console.log("\\x1b[41m test \\x1b[0m")',
+        'console.log(`x1b[41m test x1b[0m`)',
+      ].join('\n'), 'full');
+      const expectedResult = new Map([]);
+      expect(result).toEqual(expectedResult);
+    });
+
     it('should full match a full string', () => {
       const result = collectDecorationTypesWithNumRanges('console.log(`\\x1b[31m test \\x1b[0m`);', 'full');
       const expectedResult = new Map<vscode.TextEditorDecorationType, ([number, number])[]>([
