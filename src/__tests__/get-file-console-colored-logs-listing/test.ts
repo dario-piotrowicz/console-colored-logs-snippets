ed480ad6c9c22619b2ec73f047b0655403ce3319
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-11-06 13:54:12 +0000

    add listing test for indented log

--- src/__tests__/get-file-console-colored-logs-listing/test.ts
+++ src/__tests__/get-file-console-colored-logs-listing/test.ts
@@ -2,13 +2,25 @@ import * as vscode from 'vscode';
 import { getFileConsoleColoredLogsListing } from "../../get-file-console-colored-logs-listing";
 
 describe('getFileConsoleColoredLogsListing', () => {
-    it('should return a single listing for a basic file', async () => {
+    it('should return a single listing for a log at the start of a basic file', async () => {
       const listings = await getListings(
         'console.log(`\\x1b[44m test \\x1b[0m`);'
       );
       expect(listings).toEqual([
         "mockFile:1:1\n" +
         "    console.log(`\\x1b[44m test \\x1b[0m`);\n"
+      ]);
+    });
+
+    it('should return a single listing for an indented log', async () => {
+      const listings = await getListings(
+        'for(let i = 0; i < 10; i++){\n' +
+        '           console.log(`\\x1b[34m\x1b[43m ${i} \\x1b[0m`);\n' +
+        '}',
+      );
+      expect(listings).toEqual([
+        "mockFile:2:12\n" +
+        '    console.log(`\\x1b[34m\x1b[43m ${i} \\x1b[0m`);\n'
       ]);
     });
 });
